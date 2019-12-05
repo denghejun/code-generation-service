d07275953de982396044fbd3b8ba1ec351fff3df
Denghejun <hjdeng@CNhjdeng.local>
2019-12-05 17:56:10 +0800

    init

---
+++ src/main/java/com/picc/codegenerationservice/CodeGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.picc.codegenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenerationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picc/codegenerationservice/CodeGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picc.codegenerationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
